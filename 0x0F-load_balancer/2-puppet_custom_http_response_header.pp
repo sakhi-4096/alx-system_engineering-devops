ad555764271b19a9ed47e3a6bc8d8de04f05a6b5
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-04 02:42:32 +0200

    Ensure the custom HTTP header response in Nginx.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,9 @@
 # Ensure the custom HTTP header response in Nginx
 
-# Update package list
-exec { 'update':
-  command => '/usr/bin/apt-get update',
-  path    => '/usr/bin',
-  logoutput => true,
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
-
-# Install Nginx if not already installed
-package { 'nginx':
-  ensure => 'installed',
-}
-
-# Define the custom header value
-$custom_header = "add_header X-Served-By \$hostname;"
-
-# Add the custom header configuration to Nginx
-file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  line  => $custom_header,
-  match => '^http {',
-}
-
-# Restart Nginx to apply the changes
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => [Package['nginx'], File_line['http_header']],
-}
-
