c5c38cfe83047bfda4b83cc3204daa2cabced11e
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-24 00:43:00 +0200

    Add Python script to retrieve and display to-do list information for a specified employee.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""Retrieve and display a to-do list for a specific employee ID."""
+"""Retrieve and display to-do list information for a specified employee."""
+
 import requests
 import sys
 
 
-def get_employee_todo_list(employee_id):
-    base_url, user, todos = "https://jsonplaceholder.typicode.com/",
-    requests.get(f"{base_url}users/{employee_id}").json(), requests.get(
-        f"{base_url}todos", params={"userId": employee_id}).json()
-    completed_tasks, total_tasks = [t["title"]
-                                    for t in todos
-                                    if t["completed"]], len(todos)
-    print(f"Employee {user['name']} is done with tasks
-          ({len(completed_tasks)}/{total_tasks}): \n\t" +
-          "\n\t".join(completed_tasks))
+def display_employee_todos(employee_id):
+    """
+    Retrieve and display to-do list information for a specified employee.
+
+    Args:
+        employee_id (int): Employee ID for whom to retrieve to-do information
+    """
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_info = requests.get(f"{base_url}users/{employee_id}").json()
+    todos = requests.get(f"{base_url}todos", params={
+                         "userId": employee_id}).json()
+
+    completed_tasks = [task.get("title")
+                       for task in todos if task.get("completed")]
+
+    print(f"Employee {user_info.get('name')} has completed
+          {len(completed_tasks)} tasks out of {len(todos)}: ")
+    [print(f"\t{task}") for task in completed_tasks]
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <employee_id>") or sys.exit(1)
-    get_employee_todo_list(int(sys.argv[1]))
+    display_employee_todos(sys.argv[1])
