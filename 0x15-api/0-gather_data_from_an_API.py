e3d0a1c32dc307f90464eecf5f64346b5006a51e
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-24 01:09:51 +0200

    Fix output formating for the script.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,25 +5,22 @@ import requests
 import sys
 
 
-def display_employee_todos(employee_id):
+def display_employee_todos():
     """
     Retrieve and display to-do list information for a specified employee.
-
-    Args:
-        employee_id (int): Employee ID for whom to retrieve to-do information
     """
     base_url = "https://jsonplaceholder.typicode.com/"
-    user_info = requests.get(f"{base_url}users/{employee_id}").json()
+    user_info = requests.get(f"{base_url}users/{sys.argv[1]}").json()
     todos = requests.get(f"{base_url}todos", params={
-                         "userId": employee_id}).json()
+                         "userId": sys.argv[1]}).json()
 
     completed_tasks = [task.get("title")
                        for task in todos if task.get("completed")]
 
-    print(f"Employee {user_info.get('name')} has completed \
-          {len(completed_tasks)} tasks out of {len(todos)}: ")
-    [print(f"\t{task}") for task in completed_tasks]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_info.get("name"), len(completed_tasks), len(todos)))
+    [print(f"\t {task}") for task in completed_tasks]
 
 
 if __name__ == "__main__":
-    display_employee_todos(sys.argv[1])
+    display_employee_todos()
