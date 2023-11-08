5510bc857434fc4c4835255431d5af167de0579a
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-11-08 13:17:10 +0200

    Refactor the code to for correct output.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,18 +12,17 @@ def top_ten(subreddit):
     Returns the top ten titles for a given subreddit.
     If the subreddit is invalid, it returns None.
     """
-    # Define the user agent for the request
-    user_agent = "My User Agent 1.0"
-    headers = {'User-Agent': user_agent}
-    # Construct the API URL for the subreddit and specify a limit of 10 posts
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    # Send a GET request to the API and parse the response as JSON
-    response = requests.get(url, headers=headers).json()
-    # Extract the list of top ten posts from the response
-    top_ten_posts = response.get('data', {}).get('children', [])
-    if not top_ten_posts:
-        return None
-
-    # Print the titles of the top ten posts
-    for post in top_ten_posts:
-        print(post.get('data', {}).get('title'))
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/No-File-2963)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
