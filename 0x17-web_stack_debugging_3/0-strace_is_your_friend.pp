b71491ccd659947b6703de9bf7ec03897c93a6c1
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-11-08 10:50:28 +0200

    Fix typo in Apache configuration which caused a 500 error.

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix a typo in the Apache configuration, which caused it to return a 500 error.
+
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
