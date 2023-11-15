0e9a6be109563c852ed07c90aff814f22b6aff0d
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-11-15 23:23:24 +0200

    Puppet manifest to increase the amount of traffic an Nginx server can handle.

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Puppet manifest to increase the amount of traffic an Nginx server can handle.
+
+# Run the sed command to replace the ULIMIT value in the /etc/default/nginx file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
